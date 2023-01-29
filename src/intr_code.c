ea23a4f0da3c132160ed2ca175c8e66d3580e56f
Ziemas <ziemas@ziemas.se>
2023-01-29 10:43:16 +0100

    intr_code.c

--- src/intr_code.c
+++ src/intr_code.c
@@ -1,3 +1,4 @@
+#include "loader.h"
 #include "types.h"
 /* data 1308 */ bool gKillTimer = false;
 /* data 130c */ SInt32 gTransfering = 0;
@@ -14,7 +15,60 @@
 /* data 133c */ SInt32 gDoneDMASema = 0;
 /* data 1340 */ SInt32 gWaitingDMAComplete = 0;
 
-/* 0000ff74 00010008 */ SInt32 snd_TimerLLTick(/* 0x0(sp) */ struct timercommon *common) {}
-/* 00010008 000100e0 */ int snd_TransCallback(/* 0x0(sp) */ SInt32 ch, /* 0x4(sp) */ void *data) {}
-/* 000100e0 00010164 */ void snd_DoneVAGReadCB(/* 0x0(sp) */ SInt32 code) {}
-/* 00010164 0001022c */ int snd_VAGDMADone0(/* 0x0(sp) */ int ch, /* 0x4(sp) */ void *data) {}
+/* 0000ff74 00010008 */ SInt32 snd_TimerLLTick(/* 0x0(sp) */ struct timercommon *common) {
+    iWakeupThread(*common->thid);
+
+    if (gKillTimer) {
+        gKillTimer = false;
+        return 0;
+    }
+
+    return common->compare;
+}
+
+/* 00010008 000100e0 */ int snd_TransCallback(/* 0x0(sp) */ SInt32 ch, /* 0x4(sp) */ void *data) {
+    if (gTransfering == 0) {
+        return 1;
+    }
+
+    snd_FreeSPUDMA(gTransfering - 1);
+    gTransfering = 0;
+
+    if (gTransferDoneCallback != NULL) {
+        gTransferDoneCallback();
+    }
+
+    if (gSPURAMTransSema >= 0) {
+        iSignalSema(gSPURAMTransSema);
+    }
+
+    return 0;
+}
+
+/* 000100e0 00010164 */ void snd_DoneVAGReadCB(/* 0x0(sp) */ SInt32 code) {
+    gCdBusy = 0;
+    if (code != 1) {
+        gOddCdError = 1;
+        gOddCdErrorCode = code;
+    }
+
+    iSignalSema(gDoneLoadSema);
+}
+
+/* 00010164 0001022c */ int snd_VAGDMADone0(/* 0x0(sp) */ int ch, /* 0x4(sp) */ void *data) {
+    gDMAInUse[ch] = 0;
+
+    if (ch == 0) {
+        gLastDMA0Complete = 1;
+    } else {
+        gLastDMA1Complete = 1;
+    }
+
+    iSignalSema(gDoneDMASema);
+    if (gWaitingDMAComplete > 0) {
+        iWakeupThread(gWaitingDMAComplete);
+        gWaitingDMAComplete = 0;
+    }
+
+    return 0;
+}
